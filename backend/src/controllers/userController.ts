5cdf5b7c48d52429bec5bf5bbcc36d804aa049bd
R-enanVieira <renanv.guedes7@gmail.com>
2024-09-06 17:51:34 -0300

    Create UserController with method to get the authenticated user

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,23 @@
+import {Request, Response} from 'express';
+import {UserService} from '../services/userService';
+
+class UserController {
+    constructor(private userService: UserService) {}
+
+    // Get authenticated user
+    async getCurrentUser(req: Request, res: Response): Promise<void> {
+        const userId = req.user.id;
+        try {
+            const user = await this.userService.getUserById(userId);
+            res.json(user);
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({message: error.message});
+            } else {
+                res.status(400).json({message: 'An unknown error occurred'});
+            }
+        }
+    }
+}
+
+export default UserController;
